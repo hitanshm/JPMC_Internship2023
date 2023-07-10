363cff00e57b67deac51ba312215b11185c706da
ChetanGorantla <chetangorantla7@gmail.com>
2023-07-10 11:22:13 -0500

    first commit

---
+++ src/test/java/testing.java
@@ -0,0 +1,45 @@
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+import org.example.CassandraConnector;
+import org.example.KeyspaceRepository;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class testing {
+    private KeyspaceRepository schemaRepository;
+    private Session session;
+
+    @Before
+    public void connect() {
+        CassandraConnector client = new CassandraConnector();
+        client.connect("127.0.0.1", 9042);
+        this.session = client.getSession();
+        schemaRepository = new KeyspaceRepository(session);
+    }
+
+    @Test
+    public void whenCreatingAKeyspace_thenCreated() {
+        String keyspaceName = "test2";
+        schemaRepository.createKeyspace(keyspaceName, "SimpleStrategy", 1);
+
+        ResultSet result =
+                session.execute("SELECT * FROM system_schema.keyspaces;");
+
+        List<String> matchedKeyspaces = result.all()
+                .stream()
+                .filter(r -> r.getString(0).equals(keyspaceName.toLowerCase()))
+                .map(r -> r.getString(0))
+                .collect(Collectors.toList());
+
+        assertEquals(matchedKeyspaces.size(), 1);
+        assertTrue(matchedKeyspaces.get(0).equals(keyspaceName.toLowerCase()));
+    }
+
+
+}
