71790fb127fb54bfc04fe711ebca639b641ed8d7
Kavya Agar <kavyaagar0@gmail.com>
2023-07-14 20:56:30 -0500

    Base files

---
+++ src/main/java/org/example/AccountDetails.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class AccountDetails {
+    private int accountID;
+    private String name;
+    private int balance;
+
+    public AccountDetails(int accountIdentifier, String personName, int bankBalance){
+        this.accountID = accountIdentifier;
+        this.name = personName;
+        this.balance = bankBalance;
+    }
+
+}
