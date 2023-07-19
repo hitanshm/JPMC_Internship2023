4c11f2b7ac58792f4b8faa5ae6456a0441fcaae6
Shivin-Ganjoo <shivinganjoo22@gmail.com>
2023-07-19 11:41:41 -0500

    Reads Cassandra Sample Data and Writes Data in JSON Format, Connected to and Can Import Files to AWS S3

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,17 +1,18 @@
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Row;
-import com.datastax.driver.core.Session;
-import org.apache.cassandra.locator.SimpleStrategy;
-import org.example.*;
-import org.json.simple.JSONObject;
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import org.example.DataStorage;
+import org.example.CassandraConnector;
+import org.example.DataStorage;
+import org.example.KeyspaceRepository;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
 
 public class Testing {
     private Session session;
@@ -19,12 +20,14 @@ public class Testing {
     @Before
     public void connect() {
         CassandraConnector client = new CassandraConnector();
-        client.connect("127.0.0.1", 9042); this.session = client.getSession();
-        schemaRepository = new KeyspaceRepository("library",session);
+        client.connect("127.0.0.1", 9042);
+        this.session = client.getSession();
+        String keyspaceName1 = "library";
+        schemaRepository = new KeyspaceRepository(keyspaceName1,session);
+        schemaRepository.createKeyspace(keyspaceName1, "SimpleStrategy", 1);
     }
     @Test
     public void whenInsertingATable_thenInsertedCorrectly() {
-        schemaRepository.createKeyspace("library", "SimpleStrategy", 1);
 
         schemaRepository.createTable("accountdetails", "accountid","int","name","text", "balance","int");
 
@@ -40,15 +43,18 @@ public class Testing {
         assertTrue(columnNames.contains("accountid"));
         assertTrue(columnNames.contains("name"));
         assertTrue(columnNames.contains("balance"));
-        DataStorage accountDetails = new DataStorage(1,"ram",1212);
+        DataStorage accountDetails = new DataStorage(1,"ram",100);
         schemaRepository.insertRow("accountdetails", accountDetails);
-        DataStorage testad = schemaRepository.selectRow("accountdetails",1);
-        String jsonstring = schemaRepository.convertToJson(testad);
+        DataStorage accountDetails2 = new DataStorage(2,"bam",200);
+        schemaRepository.insertRow("accountdetails", accountDetails2);
+        DataStorage accountDetails3 = new DataStorage(3,"sam",300);
+        schemaRepository.insertRow("accountdetails", accountDetails3);
         schemaRepository.createfile("C:\\JPMC\\CassandraTest.txt");
-        schemaRepository.writefile("C:\\JPMC\\CassandraTest.txt",jsonstring);
-
-
-
+        String jsonstring = "";
+        for(int i=1; i<=3;i++){
+            DataStorage testad = schemaRepository.selectRow("accountdetails", i);
+            jsonstring += schemaRepository.convertToJson(testad) + "\n";
+            schemaRepository.writefile("C:\\JPMC\\CassandraTest.txt",jsonstring);
+        }
     }
 }
-
