87303e1346a1abb9d2cc94da874a8253ee46b249
Shivin-Ganjoo <shivinganjoo22@gmail.com>
2023-07-27 16:31:24 -0500

    Reads Cassandra Sample Data and Writes Data in JSON Format, Connected to and Can Import Files to AWS S3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,16 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        for (int i=0; i < 5; i++) {
-            MultithreadingTest multitest = new MultithreadingTest(i,"accountdetails");
-            multitest.start();
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
         }
     }
 }
