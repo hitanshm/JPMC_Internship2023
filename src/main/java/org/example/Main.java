68ca50a0350f25a001729c81b5a3bfdc1022522d
Shivin-Ganjoo <shivinganjoo22@gmail.com>
2023-07-28 17:09:58 -0500

    Reads Cassandra Sample Data and Writes Data in JSON Format, Connected to and Can Import Files to AWS S3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,30 @@
 package org.example;
 
+import com.datastax.driver.core.*;
+import javafx.application.Platform;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import static javafx.application.Platform.exit;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        for (int i=0; i < 5; i++) {
-            MultithreadingTest multitest = new MultithreadingTest(i,"accountdetails");
+        ArrayList<String> table_names = new ArrayList<String>();
+        Cluster cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+        Metadata metadata = cluster.getMetadata();
+        Iterator<TableMetadata> tm = metadata.getKeyspace("library").getTables().iterator();
+
+        while (tm.hasNext()) {
+            TableMetadata t = tm.next();
+            table_names.add(t.getName());
+        }
+        for (int i = 0; i < table_names.size(); i++) {
+            MultithreadingTest multitest = new MultithreadingTest(i, table_names.get(i));
             multitest.start();
         }
+        //Platform.exit();
     }
 }
