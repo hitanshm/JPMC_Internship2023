8cddef60c3b3a811bebc319e2765be477d88aac6
ChetanGorantla <chetangorantla7@gmail.com>
2023-08-01 19:05:39 -0500

    Merge branch 'chetan' of https://github.com/hitanshm/JPMC_Internship2023 into chetan

--- src/main/java/org/example/ReadS3.java
+++ src/main/java/org/example/ReadS3.java
@@ -9,10 +9,10 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
 import java.util.List;
 
 public class ReadS3 {
-    public void main(String[] args){
+    public static void main(String[] args){
         String bucket_name = "chetan-test-bucket-1";
         System.out.format("Objects in S3 bucket %s:\n", bucket_name);
-        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_2).build();
+        final AmazonS3 s3 = AmazonS3ClientBuilder.standard().withRegion(Regions.valueOf("US_EAST_2")).build();
         ListObjectsV2Result result = s3.listObjectsV2(bucket_name);
         List<S3ObjectSummary> objects = result.getObjectSummaries();
         for (S3ObjectSummary os : objects) {
